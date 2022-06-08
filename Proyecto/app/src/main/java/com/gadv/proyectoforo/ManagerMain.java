cff2a7e2de29d8587a661885084260e777b27b75
Daniela <a20110402@ceti.mx>
2022-06-07 20:22:11 -0500

    creacion de clases de activities
    creacion de clases de activities

---
+++ Proyecto/app/src/main/java/com/gadv/proyectoforo/ManagerMain.java
@@ -0,0 +1,13 @@
+package com.gadv.proyectoforo;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManagerMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager);
+    }
+}
