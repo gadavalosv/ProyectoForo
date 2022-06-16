d00b240208f46a53005d9841f00edd40f291b318
Gustavo Davalos <gadavalosv@gmail.com>
2022-06-16 10:35:27 -0500

    Adaptacion SQL a uso de Clases

--- ceti_foro.sql
+++ ceti_foro.sql
@@ -5,19 +5,19 @@ use ceti_foro;
 
 create table administrador (
 	id smallint not null AUTO_INCREMENT,
-    nombre varchar(30) not null,
-    apellidos varchar(30) not null,
-	correo varchar(40) not null,
-    contrasenia varchar(50) not null,
+    -- nombre varchar(30) not null,
+    -- apellidos varchar(30) not null,
+	-- correo varchar(40) not null,
+    -- contrasenia varchar(50) not null,
     primary key (id)
 );
 
 create table cliente (
 	id int not null AUTO_INCREMENT,
-    nombre varchar(30) not null,
-    apellidos varchar(30) not null,
-	correo varchar(40) not null,
-    contrasenia varchar(50) not null,
+    -- nombre varchar(30) not null,
+    -- apellidos varchar(30) not null,
+	-- correo varchar(40) not null,
+    -- contrasenia varchar(50) not null,
     primary key (id)
 );
 
