2524ebe211b8e438ff812e683ed4b5d9b201446a
aaronblack24 <a20110369@ceti.mx>
2022-06-20 02:53:59 -0500

    cambio

--- Proyecto/app/src/main/java/com/gadv/proyectoforo/newBlog.java
+++ Proyecto/app/src/main/java/com/gadv/proyectoforo/newBlog.java
@@ -3,12 +3,37 @@ package com.gadv.proyectoforo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class newBlog extends AppCompatActivity {
+     TextView titulo;
+     TextView contenido;
+     Button   btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_blog);
+        titulo=(TextView) findViewById(R.id.tituloNewBlog);
+        contenido=(TextView) findViewById(R.id.contenidoNewBlog);
+
+        btn1=findViewById(R.id.subirBlog);
+        btn1.setOnClickListener(new View.OnClickListener(){
+        public void onClick(View v){
+
+         insertar();
+
+
+        }
+        });
+
     }
+    private void insertar(){
+        String sTitulo=titulo.getText().toString().trim();
+        String sContenido=titulo.getText().toString().trim();
+        
+    }
+
 }
