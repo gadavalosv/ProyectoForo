349138bedeb35c53b15e7a1ca4fe0bd3777f7f3a
Ч-ƁƖƬɀ <a16100266@live.ceti.mx>
2022-12-06 18:03:21 -0600

    App menu linker
    Ready (with no retrieved id from bd to usr/admin yet)

--- Proyecto/app/src/main/java/com/gadv/proyectoforo/BanActivity.java
+++ Proyecto/app/src/main/java/com/gadv/proyectoforo/BanActivity.java
@@ -1,73 +1,34 @@
 package com.gadv.proyectoforo;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.google.android.material.navigation.NavigationView;
-
-import java.util.Objects;
-
 public class BanActivity extends AppCompatActivity {
-
-    private DrawerLayout drawerLayout;
-    private Toolbar toolbar;
-    private ActionBarDrawerToggle drawerToggle;
+    int userType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ban);
-
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
-
-        drawerLayout = findViewById(R.id.drawer_layout);
-
-        drawerToggle = setupDrawerToggle();
-        drawerToggle.setDrawerIndicatorEnabled(true);
-        drawerToggle.syncState();
-
-        drawerLayout.addDrawerListener(drawerToggle);
-
-        NavigationView navigationView = findViewById(R.id.nvView);
-        setupDrawerContent(navigationView);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (drawerToggle.onOptionsItemSelected(item))
-            return true;
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    private ActionBarDrawerToggle setupDrawerToggle() {
-        return new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open,  R.string.drawer_close);
-    }
-
-    private void setupDrawerContent(NavigationView navigationView){
-        navigationView.setNavigationItemSelectedListener(
-                menuItem -> {
-                    selectDrawerItem(menuItem);
-                    return true;
-                });
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.navigation_menu_two, menu);
+        return true;
     }
 
     @SuppressLint("NonConstantResourceId")
-    public void selectDrawerItem(MenuItem menuItem) {
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
         Intent intent;
+        int id = menuItem.getItemId();
 
-        switch(menuItem.getItemId()) {
+        switch (id) {
             case R.id.navWelcomeActivity:
                 intent = new Intent(BanActivity.this, WelcomeActivity.class);
                 startActivity(intent);
@@ -79,29 +40,15 @@ public class BanActivity extends AppCompatActivity {
                 break;
 
             case R.id.navDenyAccess:
-                Toast.makeText(getApplicationContext(), "Ya se encuentra en la pestaña restringir acceso!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Ya te encuentras en la pantalla de Restringir acceso!", Toast.LENGTH_SHORT).show();
                 break;
 
             case R.id.navLogout:
-                intent = new Intent(BanActivity.this, MainActivity.class); //Here goes logoutActivity
+                intent = new Intent(BanActivity.this, MainActivity.class);
                 startActivity(intent);
                 break;
         }
 
-        menuItem.setChecked(true);
-        setTitle(menuItem.getTitle());
-        drawerLayout.closeDrawers();
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        drawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        drawerToggle.onConfigurationChanged(newConfig);
+        return super.onOptionsItemSelected(menuItem);
     }
 }
