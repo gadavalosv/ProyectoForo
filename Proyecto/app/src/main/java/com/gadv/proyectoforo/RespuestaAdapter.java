8305d5e700a9b01fb1242bd3017bf5ab3ef6dc02
Gustavo Davalos <gadavalosv@gmail.com>
2022-12-07 00:35:58 -0600

    Respaldo temporal adapter

--- Proyecto/app/src/main/java/com/gadv/proyectoforo/RespuestaAdapter.java
+++ Proyecto/app/src/main/java/com/gadv/proyectoforo/RespuestaAdapter.java
@@ -1,24 +1,45 @@
 package com.gadv.proyectoforo;
 
+import android.content.Context;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class RespuestaAdapter extends RecyclerView.Adapter {
+import com.gadv.proyectoforo.classes.Respuesta;
+
+import java.util.List;
+
+public class RespuestaAdapter extends RecyclerView.Adapter<RespuestaAdapter.ViewHolder> {
+
+    private List<Respuesta> respuestaList;
+    private Context context;
+
+    public RespuestaAdapter(List<Respuesta> respuestaList, Context context) {
+        this.respuestaList = respuestaList;
+        this.context = context;
+    }
+
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public RespuestaAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return null;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull RespuestaAdapter.ViewHolder holder, int position) {
 
     }
 
     @Override
     public int getItemCount() {
         return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
     }
 }
